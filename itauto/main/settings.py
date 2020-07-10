0d256d9f50f0e639ba8f14f148cd6a3939cc5e6d
xuxiaowei0512 <786018072@qq.com>
2020-07-10 12:46:47 +0800

    rename

--- itauto/main/settings.py
+++ itauto/main/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'main.apps.MainConfig',
+    'api.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
