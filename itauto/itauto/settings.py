61ac4a2447f6693a575b3ee5f9bce4ee8b8c9ec9
xuxiaowei0512 <786018072@qq.com>
2020-07-10 17:23:41 +0800

    update

--- itauto/itauto/settings.py
+++ itauto/itauto/settings.py
@@ -11,10 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -31,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "apps.workbench",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
