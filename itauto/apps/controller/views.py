7f31036c9da631c9ddd873886cf2f26f3c21dbc7
xuxiaowei0512 <786018072>
2020-07-11 14:58:51 +0800

    new branch

--- itauto/apps/controller/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-
-# Create your views here.
- 
-def index(request):
-    return render(request, "controller/main.html")
