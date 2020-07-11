879d00daba2c03892bd7f050de63d7c49a500775
xuxiaowei0512 <786018072>
2020-07-11 23:58:03 +0800

    update

--- itauto/ui/views.py
+++ itauto/ui/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "index.md")
+    return render(request, "index.html")
 
 def cli(request):
-    return render(request, "index.md")
+    return render(request, "index.html")
 
 def template(request):
-    return render(request, "index.md")
+    return render(request, "index.html")
 
 def tasks(request):
-    return render(request, "index.md")
+    return render(request, "exec-tasks.html")
