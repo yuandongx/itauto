d40dd834c29022d4be6e717afe99af9820333b0e
yuandonx <786018072@qq.com>
2020-07-20 01:12:42 +0000

    rebase

--- itauto/main/settings.py
+++ itauto/main/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '34=&tb(g5yb7g!$u+h9+(8y012u0fb$*5pq^-d^b#5$cea-bxp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.56.101", "127.0.0.1"]
-
+ALLOWED_HOSTS = ["192.168.56.101", "192.168.56.102"]
 
 # Application definition
 
