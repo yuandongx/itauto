116e463db225e9078736ea36d034f7526564659f
xuxiaowei0512 <786018072@qq.com>
2020-07-21 16:50:33 +0800

    add and update

--- itauto/static/assets/scripts/utils.js
+++ itauto/static/assets/scripts/utils.js
@@ -1,20 +1,51 @@
 
 
+function confirm_select_cli_host(){
+    var rows = document.getElementById("cli-hosts-lists").rows;
+    var hosts = document.getElementById("hosts-list");
+    for(var i=1,l=rows.length; i<l;i++){
+        var node = document.createElement("span");
+        node.setAttribute("class", "label label-primary");
+		node.setAttribute("id", "host-" + rows[i].cells[0].firstChild.id);
+        node.textContent = rows[i].cells[2].textContent + "(" + rows[i].cells[1].textContent+")";
+        var ig = document.createElement("i");
+        ig.setAttribute("class", "glyphicon glyphicon-remove");
+        ig.addEventListener("click", function(){
+			var id = $(this).parent().attr("id");
+			document.getElementById("hosts-list").removeChild(document.getElementById(id));
+		});
+		node.appendChild(ig);
+        hosts.appendChild(node);
+    }
+	$("#modalHostList").modal('hide')
+}
+
+
 function getCliHosts(){
     var id = $(this).attr("id");
     $.ajax({
         url: "/tasks/cli/get-hosts",
         type: "GET",
         data: "page=9",
-        success: function(data){
-            $("#hosts-list-cli").append(
-                "<tr><th scope=‘row’><input id='op1' name='op1' type='checkbox'/></th><td>Mark</td><td>Otto</td><td>@mdo</td></tr>"
-            );
+        success: function (result) {
+            //refresh_cli_hosts(result);
+            $("#hosts-list-cli").html(result);
         },
     });
     return false;
 }
 
+function select_table_all_rows(){
+
+    var all_checked = $("#select-all-hosts").prop("checked");
+    $("#cli-hosts-lists").find("input").each(function(){
+        if(all_checked){
+            $(this).prop("checked", true);
+        } else{
+            $(this).prop("checked", false);
+        }
+    });
+}
 
 function getCookie(name) {
     let cookieValue = null;
--- itauto/task/views.py
+++ itauto/task/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 from django.contrib import messages
 from django.urls import reverse
@@ -5,7 +6,9 @@ from django.http import HttpResponseRedirect
 from .models import Task
 from host.models import Hosts
 from django.core.paginator import Paginator
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from .models import Task
+from host.models import Hosts
 # Create your views here.
 
 def add_task(request):
@@ -20,7 +23,7 @@ def add_task(request):
         )
 
     host.save()
-        # messages.add_message(request, messages.ERROR, 'Save failed.')
+    # messages.add_message(request, messages.ERROR, 'Save failed.')
     messages.add_message(request, messages.SUCCESS, 'Save successed.')
     return HttpResponseRedirect(reverse('tasks'))
 
@@ -36,4 +39,17 @@ def cli(request):
     return render(request, "tasks-shell.html")
 
 def cli_hosts(request):
-    return JsonResponse({"a": 1})
+    result = []
+    hosts = Hosts.objects.all()
+    for host in hosts:
+        result.append(
+            """<tr>
+                  <th scope="row"><input id='{host_id}' name='{host_id}' type='checkbox'/></th>
+                  <td>{host_type}</td>
+                  <td>{host_name}</td>
+                  <td>{host_ip}</td>
+                </tr>""".format(host_id=host.host_id,
+            host_type=host.host_type,
+            host_name=host.host_name,
+            host_ip=host.host_ip))
+    return HttpResponse("".join(result))
