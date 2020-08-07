3030af647dbfd535021acc0481862911ad11f0dc
yuandongx <786018072@qq.com>
2020-08-07 02:14:04 +0000

    ..

--- itauto/main/settings.py
+++ itauto/main/settings.py
@@ -82,6 +82,11 @@ ASGI_APPLICATION = 'main.routing.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mysql',
